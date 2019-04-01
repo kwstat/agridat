376e43bba823a6aab46f430ace90a49d8c152279
Kevin Wright <kw.stat@gmail.com>
2019-03-31 23:01:42 -0500

    data: update laycock.tea.uniformity with additional location

--- data-done/laycock.tea.R
+++ data-done/laycock.tea.R
@@ -4,19 +4,26 @@ libs(dplyr,readxl,reshape2,tidyverse)
 
 setwd("c:/x/rpack/agridat/data-done/")
 
-dat <- read_csv("laycock.tea.csv", col_names=FALSE)
+dat1 <- read_excel("laycock.tea.xlsx", sheet=1, col_names=FALSE)
+dat2 <- read_excel("laycock.tea.xlsx", sheet=2, col_names=FALSE)
 
-dat <- dat %>% as.matrix %>%
-  `rownames<-`(1:nrow(dat)) %>% `colnames<-`(1:ncol(dat)) %>%
+dat1 <- dat1 %>% as.matrix %>%
+  `rownames<-`(1:nrow(dat1)) %>% `colnames<-`(1:ncol(dat1)) %>%
+  melt %>% rename(row=Var1,col=Var2,yield=value)
+dat2 <- dat2 %>% as.matrix %>%
+  `rownames<-`(1:nrow(dat2)) %>% `colnames<-`(1:ncol(dat2)) %>%
   melt %>% rename(row=Var1,col=Var2,yield=value)
 
-laycock.tea <- dat
+dat1 <- cbind(loc="L1", dat1)
+dat2 <- cbind(loc="L2", dat2)
+
+laycock.tea.uniformity <- rbind(dat1,dat2)
 
 # ----------------------------------------------------------------------------
 
-dat <- laycock.tea
+dat <- laycock.tea.uniformity
 
 if(require(desplot)){
   desplot(yield ~ col*row, dat,
-          main="laycock.tea", flip=TRUE)
+          main="laycock.tea.uniformity", flip=TRUE)
 }
