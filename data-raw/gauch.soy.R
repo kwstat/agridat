2cac66094ba414d5b34091f0956c836134972c41
Kevin Wright <kw.stat@gmail.com>
2020-07-10 17:00:39 -0500

    gauch.soy updated

---
+++ data-raw/gauch.soy.R
@@ -0,0 +1,17 @@
+
+libs(agridat)
+
+dat <- gauch.soy
+dat <- mutate(dat,
+              year=substring(dat$env, 2),
+              loc=substring(dat$env, 1, 1) )
+  are C=Chazy, N=Canton, L=Lockport, G=Geneseo, R=Romulus, A=Aurora,
+  I=Ithica, V=Valatie, D=Riverhead.
+
+dat <-
+  mutate(dat,
+              loc=recode(loc,
+                         A="Aurora", C="Chazy", E="Etna",N="Canton", L="Lockport",G="Geneseo",R="Romulus",I="Itica",V="Valatie",D="Riverhead"))
+gauch.soy <- dat
+write.table(gauch.soy, file = "c:/x/rpack/agridat/data/gauch.soy.txt",
+            sep = "\t", row.names = FALSE)
