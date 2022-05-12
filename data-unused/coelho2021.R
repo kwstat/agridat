3ecab98fa037a3cef4579f44605c4639aef244ef
Kevin Wright <kw.stat@gmail.com>
2022-05-12 13:42:26 -0500

    data coelho2021

---
+++ data-unused/coelho2021.R
@@ -0,0 +1,14 @@
+
+# Coelho et al 2021
+# Accounting for spatial trends in multi-environment diallel analysis in maize breeding
+# https://doi.org/10.1371/journal.pone.0258473
+
+# Mildly interesting data that has spatial coordinates for a diallel at 4 locs.
+# The paper shows no heatmaps!
+
+setwd("c:/one/rpack/agridat/data-raw")
+dat <- read.csv("coelho2021.txt", sep="\t")
+head(dat)
+
+libs(desplot)
+desplot(dat, GY ~ Columns*Rows|Trials, out1=Replications)
